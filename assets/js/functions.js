dfb17c0dd7726a40e9a536d2434b4a3225d82d85
hannestee <tikkha1@gmail.com>
2021-04-12 20:40:46 +0300

    revamp site

--- assets/js/functions.js
+++
@@ -1,6 +0,0 @@
-$('.toggled-nav-wrapper').on('click', function(e){
-  e.stopPropoagation();
-});
-$('.nav-toggle-open-wrapper, .nav-toggle-close-wrapper, header .nav-toggle-body-overlay').on('click', function(){
-  $('html').toggleClass('nav-open');
-})
